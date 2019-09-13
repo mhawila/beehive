9374c2776e5e92924842670a611dcd74dba875c8
Willa Mhawila <a.mhawila@gmail.com>
2019-09-13 11:35:26 -0500

    Updating config.json working

--- web/server.js
+++ web/server.js
@@ -4,10 +4,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const orchestration = require('../orchestration')
 const MessageQueue = require('../MessageQueue');
+const nodeUtils = require('../node_utils')
+
 global.progressMessageQueue = new MessageQueue();
 
 const port = 3000
-const CONFIG_DIR = '../';
+const CONFIG_DIR = require('path').dirname(__dirname) + '/';
+const CONFIG_FILE_PATH = CONFIG_DIR + '/config.json';
 
 const app = express()
 const ws = require('express-ws')(app);
@@ -20,20 +23,25 @@ app.use(bodyParser.json())
 
 app.route('/configuration')
     .get((req, res) => {
-        let config = require('../config')
+        let config = require(CONFIG_FILE_PATH)
         res.json(config)
     })
     .post((req, res) => {
         // Archive the existing configuration if any.
-        // let existingConfig = require('../config')
-        // if(existingConfig) {
-        //     let configArchiveDir = CONFIG_DIR + 'config-archive';
-        //     if(!fs.existsSync(configArchiveDir)) {
-        //         fs.mkdirSync(configArchiveDir, '0755')
-        //     }
-        //     console.log('nothing happening here...')
-        // }
-        console.log('Configuration:', req.body)
+        let existingConfig = require(CONFIG_FILE_PATH)
+        if(existingConfig) {
+            let configArchiveDir = CONFIG_DIR + 'config-archives';
+            if(!fs.existsSync(configArchiveDir)) {
+                fs.mkdirSync(configArchiveDir, '0755')
+            }
+            let configCopy = 'config_' + new Date().toISOString() + '.json';
+            fs.copyFileSync(CONFIG_FILE_PATH, configArchiveDir + '/' + configCopy)
+        }
+        fs.writeFileSync(CONFIG_FILE_PATH, JSON.stringify(req.body, null, 2))
+        // Purge the config from require cache.
+        nodeUtils.removeModuleFromRequireCache(CONFIG_FILE_PATH)
+
+        // console.log('Configuration:', req.body)
         res.json({status: 'success'})
     })
 
