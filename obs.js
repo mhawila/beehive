42e50db1fac98bdec6b1b62113c25088e17d3fc5
Willa Mhawila <a.mhawila@gmail.com>
2020-11-02 00:58:33 -0600

    BEEHIVE-43: Replaced .forEach with regular for loop

--- obs.js
+++ obs.js
@@ -16,50 +16,51 @@ function prepareObsInsert(rows, nextId) {
         + 'date_voided, void_reason, uuid) VALUES ';
 
   let toBeinserted = '';
-  rows.forEach(row => {
+  for(let index = 0; i < rows.length; i++) {
+    let row = rows[i];
     if(toBeinserted.length > 1) {
-      toBeinserted += ',';
-    }
-
-    let voidedBy = row['voided_by'] === null ? null : beehive.userMap.get(row['voided_by']);
-    let obsGroupsId = row['obs_group_id'] === null ? null : beehive.obsMap[row['obs_group_id']];
-    let previous = row['previous_version']=== null ? null : beehive.obsMap[row['previous_version']];
-    let encounterId = row['encounter_id'] === null ? null : beehive.encounterMap.get(row['encounter_id']);
-    let locationId = row['location_id'] === null ? null : beehive.locationMap.get(row['location_id']);
-    beehive.obsMap[row['obs_id']] = nextId;
-
-    if(obsGroupsId === undefined) {
-        obsGroupsId = null;
-        if(row['obs_group_id'] !== null) {
-            obsWithTheirGroupNotUpdated[nextId] = row['obs_group_id'];
-        }
-    }
-
-    if(previous === undefined) {
-        previous = null;
-        if(row['previous_version'] !== null) {
-            obsWithPreviousNotUpdated[nextId] = row['previous_version'];
-        }
-    }
-
-    toBeinserted += `(${nextId}, ${beehive.personMap.get(row['person_id'])}, `
-        + `${row['concept_id']},  ${encounterId}, `
-        + `${row['order_id']}, ${strValue(utils.formatDate(row['obs_datetime']))}, `
-        + `${locationId}, ${obsGroupsId}, `
-        + `${strValue(row['accession_number'])}, ${row['value_group_id']}, `
-        + `${row['value_boolean']}, ${row['value_coded']}, `
-        + `${row['value_coded_name_id']}, ${row['value_drug']}, `
-        + `${strValue(utils.formatDate(row['value_datetime']))}, `
-        + `${row['value_numeric']}, ${strValue(row['value_modifier'])}, `
-        + `${strValue(row['value_text'])}, ${strValue(row['value_complex'])}, `
-        + `${strValue(row['comments'])}, ${previous}, `
-        + `${beehive.userMap.get(row['creator'])}, `
-        + `${strValue(utils.formatDate(row['date_created']))}, `
-        + `${row['voided']}, ${voidedBy}, ${strValue(utils.formatDate(row['date_voided']))}, `
-        + `${strValue(row['void_reason'])}, ${utils.uuid(row['uuid'])})`
-
-    nextId++;
-  });
+        toBeinserted += ',';
+      }
+  
+      let voidedBy = row['voided_by'] === null ? null : beehive.userMap.get(row['voided_by']);
+      let obsGroupsId = row['obs_group_id'] === null ? null : beehive.obsMap[row['obs_group_id']];
+      let previous = row['previous_version']=== null ? null : beehive.obsMap[row['previous_version']];
+      let encounterId = row['encounter_id'] === null ? null : beehive.encounterMap.get(row['encounter_id']);
+      let locationId = row['location_id'] === null ? null : beehive.locationMap.get(row['location_id']);
+      beehive.obsMap[row['obs_id']] = nextId;
+  
+      if(obsGroupsId === undefined) {
+          obsGroupsId = null;
+          if(row['obs_group_id'] !== null) {
+              obsWithTheirGroupNotUpdated[nextId] = row['obs_group_id'];
+          }
+      }
+  
+      if(previous === undefined) {
+          previous = null;
+          if(row['previous_version'] !== null) {
+              obsWithPreviousNotUpdated[nextId] = row['previous_version'];
+          }
+      }
+  
+      toBeinserted += `(${nextId}, ${beehive.personMap.get(row['person_id'])}, `
+          + `${row['concept_id']},  ${encounterId}, `
+          + `${row['order_id']}, ${strValue(utils.formatDate(row['obs_datetime']))}, `
+          + `${locationId}, ${obsGroupsId}, `
+          + `${strValue(row['accession_number'])}, ${row['value_group_id']}, `
+          + `${row['value_boolean']}, ${row['value_coded']}, `
+          + `${row['value_coded_name_id']}, ${row['value_drug']}, `
+          + `${strValue(utils.formatDate(row['value_datetime']))}, `
+          + `${row['value_numeric']}, ${strValue(row['value_modifier'])}, `
+          + `${strValue(row['value_text'])}, ${strValue(row['value_complex'])}, `
+          + `${strValue(row['comments'])}, ${previous}, `
+          + `${beehive.userMap.get(row['creator'])}, `
+          + `${strValue(utils.formatDate(row['date_created']))}, `
+          + `${row['voided']}, ${voidedBy}, ${strValue(utils.formatDate(row['date_voided']))}, `
+          + `${strValue(row['void_reason'])}, ${utils.uuid(row['uuid'])})`
+  
+      nextId++;
+  }
 
   let insertStatement = insert + toBeinserted;
   return [insertStatement, nextId];
